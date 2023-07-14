e5d71166e0ae682dcfcd83604e2fc9cf8b27d28d
Clayton Barnes <barnes.clayton@icloud.com>
2023-07-14 14:30:46 -0700

    add alignment function

---
+++ CodeForWoodsHole2023/General/alignBehaviorData.m
@@ -0,0 +1,25 @@
+function alignedData = alignBehaviorData(mesoTimestamps,behaviorData,behaviorTimestamps)
+% want to match face camera to meso data 
+% find nearest meso timestamp to each face camera timestamp and get the index of the nearest meso timestamp 
+alignedData = nan(size(mesoTimestamps));
+
+for i =1:length(behaviorData)
+    % fast method to find what ind of mesoTimestamps behaviorTimestamps(i)
+    % is >=
+    ind = findInSorted(mesoTimestamps,behaviorTimestamps(i));
+    % check to see if timestamps is closer to found ind or previous
+    if ind > 1
+        [~,ind2] = min([abs(mesoTimestamps(ind)-behaviorTimestamps(i)),abs(mesoTimestamps(ind-1)-behaviorTimestamps(i))]);
+        temp = [ind ind-1];
+        ind = temp(ind2);
+    end
+    alignedData(ind) = behaviorData(i);
+end
+
+% interpolate 
+nanx = isnan(alignedData);
+t    = 1:numel(alignedData);
+alignedData(nanx) = interp1(t(~nanx), alignedData(~nanx), t(nanx));
+
+
+
